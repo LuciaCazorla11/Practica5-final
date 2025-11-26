957371640e9fb8af33ee1892903b34afe2690b00
LuciaCazorla <lucazorlaortega@uma.es>
2025-11-26 16:02:24 +0100

    Initial commit: Proyecto base configurado

---
+++ src/main/java/es/grupob/practica5/Practica5GrupoBApplication.java
@@ -0,0 +1,13 @@
+package es.grupob.practica5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica5GrupoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica5GrupoBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/grupob/practica5/Practica5GrupoBApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grupob.practica5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica5GrupoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

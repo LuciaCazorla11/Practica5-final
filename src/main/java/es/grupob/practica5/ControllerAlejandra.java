543d0358c854296c70890b829651e91572d29c81
AleVinSan <alevineglas@uma.es>
2025-11-26 17:58:56 +0100

    Controlador Hola Mundo de Alejandra

---
+++ src/main/java/es/grupob/practica5/ControllerAlejandra.java
@@ -0,0 +1,13 @@
+package es.grupob.practica5;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController 
+public class ControllerAlejandra {
+
+    @GetMapping("/holamundo")
+    public String saludar() {
+        return "¡Hola mundo! Soy Alejandra y este es el controlador de mi rama de desarrollo.";
+    }
+}
